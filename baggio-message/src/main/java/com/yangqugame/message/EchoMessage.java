a43dc6d6d9a5397d24dc5f6823fc79204da2de8a
Ging.wu <wu.tongqing@gmail.com>
2017-08-24 14:29:29 +0800

    add rpc server to message

--- baggio-message/src/main/java/com/yangqugame/message/EchoMessage.java
+++ baggio-message/src/main/java/com/yangqugame/message/EchoMessage.java
@@ -1,9 +1,9 @@
 package com.yangqugame.message;
 
+import com.yangqugame.service.EchoService;
 import jazmin.core.Jazmin;
 import jazmin.driver.rpc.JazminRpcDriver;
 import jazmin.server.msg.Context;
-import jazmin.server.msg.Service;
 
 /**
  * Created by ging on 07/07/2017.
@@ -13,7 +13,9 @@ import jazmin.server.msg.Service;
 public class EchoMessage {
 
     public void echo(Context context, String msg) {
-        context.ret("echo:" + msg);
+        JazminRpcDriver rpcDriver = Jazmin.getDriver(JazminRpcDriver.class);
+        EchoService service = rpcDriver.create(EchoService.class, "cluster");
+        context.ret("echo:" + service.echo(msg));
     }
 
 }

2378d182c2b0cb38eea5e77132d309b6cf32ddc0
phiau <tsengphiautshua@gmail.com>
2017-10-12 21:10:27 +0800

    把 Jazmin 的启动搬到继承开发环境，使得容易调试

---
+++ baggio-start/src/main/java/StartApplication.java
@@ -0,0 +1,19 @@
+import com.yangqugame.BaggioMessageApplication;
+import jazmin.core.Jazmin;
+import jazmin.server.protobuf.ProtobufServer;
+
+/**
+ * Created by phiau on 2017/10/12 0012.
+ */
+public class StartApplication {
+
+    public static void main(String[] args) {
+        Thread shutdownThread=new Thread(Jazmin::stop);
+        shutdownThread.setName("ShutdownHook");
+        Runtime.getRuntime().addShutdownHook(shutdownThread);
+        Jazmin.dumpLogo();
+        Jazmin.addServer(new ProtobufServer());
+        Jazmin.loadApplication(new BaggioMessageApplication());
+        Jazmin.start();
+    }
+}
